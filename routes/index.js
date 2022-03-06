e7d93111bc3cb9209801160d81a1fcca95e58e09
LanJiang <30459778+Lan-Jiang@users.noreply.github.com>
2022-03-05 23:15:40 -0500

    WEB502 LAB M3W1D3

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {check, validationResult} = require('express-validator');
+const { check, validationResult } = require('express-validator');
 
 router.get('/', function(req, res) {
     res.render('form', { title: 'Registration form' });
@@ -9,26 +9,27 @@ router.get('/', function(req, res) {
 router.post('/', 
     [
     check('name')
-        .isLength({min:1})
+        .isLength({ min: 1 })
         .withMessage('Please enter a name'),
     check('email')
-        .isLength({min:1})
+        .isLength({ min: 1})
         .withMessage('Please enter an email'),
     ],
     function(req, res) {
-        // console.log(req.body);
-        const errors = validationResult(req);
-        if (errors.isEmpty()) {
-            res.send('Thank you for your registration!');
-        } else {
-            res.render('form', {
-                title: 'Registration form',
-                errors: errors.array(),
-                data:req.body,
-            });
-        }
+        console.log(req.body);
+        // const errors = validationResult(req);
+        // if (errors.isEmpty()) {
+        //     res.send('Thank you for your registration!');
+        // } else {
+        //     res.render('form', {
+        //         title: 'Registration form',
+        //         errors: errors.array(),
+        //         data:req.body,
+        //     });
+        // }
+        res.render('form', { title: 'Registration form' });
+
         });
-        //res.render('form', { title: 'Registration form' });
 
 module.exports = router;
 // module.exports = router;
