1c90837e8b42c8bfc751ba99e68f596467e98a42
LanJiang <30459778+Lan-Jiang@users.noreply.github.com>
2022-03-14 23:33:37 -0700

    WEB502 LAB M3 W1 D3

--- routes/index.js
+++ routes/index.js
@@ -6,30 +6,36 @@ router.get('/', function(req, res) {
     res.render('form', { title: 'Registration form' });
 });
 
-router.post('/', 
+router.post ('/', 
     [
     check('name')
-        .isLength({ min: 1 })
-        .withMessage('Please enter a name'),
+        .isLength({ min:1 })
+        .withMessage ('Please enter a name'),
     check('email')
-        .isLength({ min: 1})
+        .isLength({ min:1 })
         .withMessage('Please enter an email'),
     ],
-    function(req, res) {
-        //console.log(req.body);
-        const errors = validationResult(req);
-        if (errors.isEmpty()) {
-            res.send('Thank you for your registration!');
-        } else {
-            res.render('form', {
-                title: 'Registration form',
-                errors: errors.array(),
-                data:req.body,
-            });
-        }
-        // res.render('form', { title: 'Registration form' });
-
+    function(req,res) {
+    // console.log(req.body);
+    const errors = validationResult(req);
+    if (errors.isEmpty()) {
+        // res.send('Thank you for your registration!');
+        const registration = new Registration(req.body);
+        registration.save()
+        .then(() => {res.send('Thank you for your registration!');})
+        .catch((err) => {
+            console.log(err);
+            res.send('Sorry! Something went wrong.');
         });
+    } else {
+        res.render('form', { 
+            title: 'Registration form',
+            errors: errors.array(),
+            data: req.body,
+        });
+    }
+    
+});
 
 module.exports = router;
 // module.exports = router;
